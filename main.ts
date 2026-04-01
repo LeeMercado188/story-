ff717b423183f12ccf865c010386b5b3110d5830
LeeMercado188 <leemercado42@yahoo.com>
2026-04-01 12:53:19 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -70,7 +70,7 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . 4 e e f f f f f f e . . . 
         . . . . . . . . . f f f . . . . 
         `],
-    500,
+    100,
     characterAnimations.rule(Predicate.MovingUp)
     )
 })
@@ -146,7 +146,7 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         . . f f f f f f f f f f . . . . 
         . . . f f f . . . f f . . . . . 
         `],
-    500,
+    100,
     characterAnimations.rule(Predicate.MovingLeft)
     )
 })
@@ -222,7 +222,7 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . f f f f f f f f f f . . 
         . . . . . f f . . . f f f . . . 
         `],
-    500,
+    100,
     characterAnimations.rule(Predicate.MovingRight)
     )
 })
@@ -298,7 +298,7 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . f f f f f f f . . . . 
         . . . . . . . . . f f f . . . . 
         `],
-    500,
+    100,
     characterAnimations.rule(Predicate.MovingDown)
     )
 })
@@ -309,7 +309,141 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sp
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorOpenNorth, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level2`)
     tiles.placeOnTile(PLAYER_1, tiles.getTileLocation(3, 13))
+    Inside = false
+    Town = true
+    citizens = [
+    img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 6 6 f f f . . . . 
+        . . . f f f 6 6 6 6 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 6 6 6 6 6 6 e e f . . 
+        . . f e 6 f f f f f f 6 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 6 6 6 6 6 6 f 4 e . . 
+        . . 4 d f 6 6 6 6 6 6 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,
+    img`
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f f . . . . . 
+        . . . . f 1 5 2 5 1 e f . . . . 
+        . . . f 1 e e e e e 1 e f . . . 
+        . . . f e e f f f f e 1 f . . . 
+        . . . f e f 1 d d 1 f e f . . . 
+        . . f e f d f d d f d f e f . . 
+        . . f e f d 3 d d 3 d f e f . . 
+        . . f e e f d d d d f e e f . . 
+        . f e e f 9 f f f f 9 f e e f . 
+        . . f f d 9 5 9 9 5 9 d f f . . 
+        . . f d d f 9 5 5 9 f d d f . . 
+        . . . f f 9 9 9 9 9 9 f f . . . 
+        . . . f 9 9 5 9 9 5 9 9 f . . . 
+        . . . f f f f f f f f f f . . . 
+        . . . . . f f . . f f . . . . . 
+        `,
+    img`
+        . . . . . f f 4 4 f f . . . . . 
+        . . . . f 5 4 5 5 4 5 f . . . . 
+        . . . f e 4 5 5 5 5 4 e f . . . 
+        . . f b 3 e 4 4 4 4 e 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+        . f 3 3 f f e e e e f f 3 3 f . 
+        . f b b f b f e e f b f b b f . 
+        . f b b e 1 f 4 4 f 1 e b b f . 
+        f f b b f 4 4 4 4 4 4 f b b f f 
+        f b b f f f e e e e f f f b b f 
+        . f e e f b 2 2 2 2 b f e e f . 
+        . . e 4 c 2 2 2 2 2 2 c 4 e . . 
+        . . e f b 2 b 2 b 2 b b f e . . 
+        . . . f f 1 2 1 2 1 2 f f . . . 
+        . . . . . f f b b f f . . . . . 
+        `,
+    img`
+        . . . . f f f f . . . . . 
+        . . f f f f f f f f . . . 
+        . f f f f f f c f f f . . 
+        f f f f f f c c f f f c . 
+        f f f c f f f f f f f c . 
+        c c c f f f e e f f c c . 
+        f f f f f e e f f c c f . 
+        f f f b f e e f b f f f . 
+        . f 4 1 f 4 4 f 1 4 f . . 
+        . f e 4 4 4 4 4 4 e f . . 
+        . f f f e e e e f f f . . 
+        f e f b 7 7 7 7 b f e f . 
+        e 4 f 7 7 7 7 7 7 f 4 e . 
+        e e f 6 6 6 6 6 6 f e e . 
+        . . . f f f f f f . . . . 
+        . . . f f . . f f . . . . 
+        `,
+    img`
+        . f f f . f f f f . f f f . 
+        f f f f f c c c c f f f f f 
+        f f f f b c c c c b f f f f 
+        f f f c 3 c c c c 3 c f f f 
+        . f 3 3 c c c c c c 3 3 f . 
+        . f c c c c 4 4 c c c c f . 
+        . f f c c 4 4 4 4 c c f f . 
+        . f f f b f 4 4 f b f f f . 
+        . f f 4 1 f d d f 1 4 f f . 
+        . . f f d d d d d d f f . . 
+        . . e f e 4 4 4 4 e f e . . 
+        . e 4 f b 3 3 3 3 b f 4 e . 
+        . 4 d f 3 3 3 3 3 3 c d 4 . 
+        . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+        . . . . f f f f f f . . . . 
+        . . . . f f . . f f . . . . 
+        `,
+    img`
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f f e e e e e e f f . 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        . f e d d b b d d e f . 
+        . f f e 4 4 4 4 e f f . 
+        e 4 f b 1 1 1 1 b f 4 e 
+        4 d f 1 1 1 1 1 1 f d 4 
+        4 4 f 6 6 6 6 6 6 f 4 4 
+        . . . f f f f f f . . . 
+        . . . f f . . f f . . . 
+        `,
+    img`
+        . . . . . f f f f . . . . . 
+        . . . f f 5 5 5 5 f f . . . 
+        . . f 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 f . 
+        . f 5 5 5 d b b d 5 5 5 f . 
+        f 5 5 5 b 4 4 4 4 b 5 5 5 f 
+        f 5 5 c c 4 4 4 4 c c 5 5 f 
+        f b b f b f 4 4 f b f b b f 
+        f b b 4 1 f d d f 1 4 b b f 
+        . f b f d d d d d d f b f . 
+        . f e f e 4 4 4 4 e f e f . 
+        . e 4 f 6 9 9 9 9 6 f 4 e . 
+        . 4 d c 9 9 9 9 9 9 c d 4 . 
+        . 4 f b 3 b 3 b 3 b b f 4 . 
+        . . f f 3 b 3 b 3 3 f f . . 
+        . . . . f f b b f f . . . . 
+        `
+    ]
 })
+let people: Sprite = null
+let citizens: Image[] = []
+let Town = false
+let Inside = false
 let PLAYER_1: Sprite = null
 PLAYER_1 = sprites.create(img`
     . . . . . . f f f f . . . . . . 
@@ -332,3 +466,10 @@ PLAYER_1 = sprites.create(img`
 scene.cameraFollowSprite(PLAYER_1)
 controller.moveSprite(PLAYER_1)
 tiles.setCurrentTilemap(tilemap`level1`)
+game.onUpdateInterval(100, function () {
+    if (Town && !(Inside)) {
+        people = sprites.createProjectileFromSide(citizens[randint(0, citizens.length - 1)], randint(-50, 50), randint(-50, 50))
+    } else {
+        Inside = true
+    }
+})
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -64,17 +64,19 @@ namespace myTiles {
 `, [myTiles.transparency16], TileScale.Sixteen);
             case "level1":
             case "level1":return tiles.createTilemap(hex`0a000800010808080a080808080207090909090909090906070909090909090909060108090209090909090607090906090909090906070909060909090909060709090609090909090603050504050505050504`, img`
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
+2 2 2 2 . 2 2 2 2 2 
+2 . . . . . . . . 2 
+2 . . . . . . . . 2 
+2 2 . 2 . . . . . 2 
+2 . . 2 . . . . . 2 
+2 . . 2 . . . . . 2 
+2 . . 2 . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 
 `, [myTiles.transparency16,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.floorLight2,sprites.dungeon.doorOpenNorth], TileScale.Sixteen);
             case "level2":
-            case "level2":return tiles.createTilemap(hex`1000100002020101020202020202020202020201020202020202020202020202020202020202020202020202020202020101010202020201010202020202020202020202020202020202020202020202020202020202020201020202020201020201010102010102020201020201020202020202020202020202020202020202020202020202010202020202020202010102020202010202020202020202020202010102020202020202020202020202020202010102020202020202020202020202020201010202020202020202020202020202020202020202020202010101010202010101020201020201020202020202020101020202020202020202020101010101`, img`
+            case "level2":return tiles.createTilemap(hex`1000100002020101020209080808080808080d010202020202020a0c0c0f0c0c0c0c0b0202020202020202020e0e0e020101010202020201010202020e0e0e020202020202020202020202020e0e0e020202020202020202010202020e0e010202010101020101020202010e0e0e0202020202020202020202020e0e0e0e02020202020202020102020e0e0e0e0e020101020202020102020e0e0e0e0202020202010102020202020e0e0e0202020202020202010102020e0e0e020202020202020202020101020e0e0e0202020202020202020202020e0e0e0e0e0202010101010202010507070307070401020202020202020109080808080806020202020101010101`, img`
+. . . . . . 2 2 2 2 2 2 2 2 2 . 
+. . . . . . 2 2 2 . 2 2 2 2 2 . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -87,11 +89,9 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tileGrass1], TileScale.Sixteen);
+2 2 2 2 2 2 2 . . . . . . . . . 
+2 2 2 2 2 2 2 . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tileGrass1,sprites.dungeon.doorClosedSouth,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.floorDark2,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterWest1,sprites.castle.tilePath5,sprites.dungeon.doorClosedNorth], TileScale.Sixteen);
         }
         return null;
     })
